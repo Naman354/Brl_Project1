76ead422518a67843e946fc346422a5b50b62368
Naman354 <srinaman05@gmail.com>
2025-09-20 19:14:07 +0530

    Added Patch Route and corrected the API Documentation

--- routes/Routes.js
+++ routes/Routes.js
@@ -5,10 +5,12 @@ const postRoute = require("../controllers/postRoute");
 const getRoute = require("../controllers/getRoute");
 const putRoute = require("../controllers/putRoute");
 const deleteRoute = require("../controllers/deleteRoute");
+const patchRoute = require("../controllers/patchRoute");
 
 router.post("/postRoute", postRoute);
 router.get("/getRoute", getRoute);
 router.put("/putRoute/:id" , putRoute);
 router.delete("/deleteRoute/:id" , deleteRoute);
+router.patch("/patchRoute/:id" , patchRoute);
 
 module.exports = router;
